870ee9c090a4adb2fe81188d3b8f88766771af8a
Ronca <michironca08@gmail.com>
2025-11-10 13:23:16 +0100

    funzionante

--- aeb.c
+++ aeb.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(){
-    printf("%d", getpid());
+    printf("pid : %d\n", getpid());
     int pid = fork();
     if(pid == 0){
-        printf("Processo figlio");
+        int a = 10;
+        int b = 5;       
+        printf("Processo figlio\n");
+        printf("Figlio: PID = %d, PID padre = %d\n", getpid(), getppid());
         sleep(2);
-        
+        a = a * b;
+        printf("Risultato figlio: %d\n", a);
+        _exit(42);
     }else{
-        printf(" - padre %d\n", getpid());
+        printf("Processo padre\n");
+        printf("Padre: PID = %d, PID padre = %d\n", getpid(), pid);
+        int status;
+        if (wait(&status) == -1){
+            perror("wait");
+            exit(EXIT_FAILURE);
+        }
+        if(WIFEXITED(status)){
+            printf("Padre: figlio terminato normalmente\n");
+            printf("Padre: codice di uscita = %d\n", WEXITSTATUS(status));
+        } else if(WIFSIGNALED(status)){
+            printf("Padre: figlio terminato da segnale\n");
+            printf("Padre: segnale = %d\n", WTERMSIG(status));
+        } else {
+            printf("Padre: figlio terminato in modo imprevisto, status = 0x%x\n", status);
+        }
     }
     return 0;
 }
