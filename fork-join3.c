228ce342237dbad4c8d819d5064105804bf6c0b4
Ronca <michironca08@gmail.com>
2025-11-17 13:30:02 +0100

    .

--- fork-join3.c
+++ fork-join3.c
@@ -4,34 +4,49 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+int prog1(){
+    int a = 5;
+    int b = 3;
+    a = a * b;
+    printf("Risultato 1: %d\n", a);
+    return a;
+}
+
+int prog2(){
+    int a = 9;
+    int b = 4;       
+    a = a * b;
+    printf("Risultato 2: %d\n", a);
+    return a;
+}
+
+int prog3(){
+    int a = 4;
+    int b = 7;       
+    a = a * b;
+    printf("Risultato 3: %d\n", a);
+    return a;
+}
+
 int main(){
     int pid = fork();
     if(pid == 0){     
         int proc3 = fork();
         if(proc3 == 0){
-            int a = 5;
-            int b = 3;
-            a = a * b;
-            printf("Risultato 1: %d\n", a);
-            exit(a);
+            exit(prog1());
         }       
         int ris_1;
         wait(&ris_1);
-            int a = 9;
-            int b = 4;
-            a = a * b;
-            printf("Risultato 2: %d\n", a);
-            a = a + WEXITSTATUS(ris_1);
+            int a;
+            a = prog2() + WEXITSTATUS(ris_1);
         exit(a);
     }else{
         int ris_2;
         wait(&ris_2);
-            int a = 7;
-            int b = 4;
-            a = a * b;
-            printf("Risultato 2: %d\n", a);
-            a = a + WEXITSTATUS(ris_2);    
+            int a;
+            a = prog3() + WEXITSTATUS(ris_2);    
             printf("Risultato finale: %d\n", a);    
     }
     return 0;
 }
+
