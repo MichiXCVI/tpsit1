fdeb9043ae486baa64d9c105f5b1f27ec11a741e
Ronca <michironca08@gmail.com>
2025-11-17 12:27:24 +0100

    .

--- fork-join.c
+++ fork-join.c
@@ -5,5 +5,22 @@
 #include <sys/wait.h>
 
 int main(){
-    
+    int pid = fork();
+    if(pid == 0){
+        int a = 5;
+        int b = 3;       
+        a = a * b;
+        printf("Figlio: risultato = %d\n", a);
+        _exit(a);
+    }else{
+        int a = 9;
+        int b = 4; 
+        a = a * b;
+        int result_figlio;
+        wait(&result_figlio);
+        int result = WEXITSTATUS(result_figlio) + a;
+        printf("Padre: risultato = %d\n", a);
+        printf("Risultato = %d\n", result);
+        return 0;
+    }
 }
