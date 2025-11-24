087f93435d067b3000b08fb1fcff9574d24e3116
Ronca <michironca08@gmail.com>
2025-11-24 13:02:42 +0100

    .

--- stampa.c
+++ stampa.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void incrementa(int *valore){
-    (*valore)++;
+int* incrementa(){
+    int* p = malloc(sizeof(int));
+    *p = 10;
+    return p;
 }
 
 int main(){
-    int k = 0;
-    incrementa(&k);
-    printf("Valore di k: %d\n", k);
+    int* k = 0;
+    k = incrementa();
+    printf("Valore di k: %d\n", *k);
     return 0;
 }   
